094db51d46cdfb16a0bee53037138375a851bffd
VictorGus <retaow@gmail.com>
2020-05-30 12:54:45 +0300

    Fix deployment

--- src/app/core.clj
+++ src/app/core.clj
@@ -85,7 +85,7 @@
     (reset! state nil)))
 
 (defn start-server []
-  (reset! state (server/run-server app {:port (or (Integer. )(System/getenv "PORT") 5000)})))
+  (reset! state (server/run-server app {:port (or (Integer. (System/getenv "PORT")) 5000)})))
 
 (defn restart-server [] (stop-server) (start-server))
 
